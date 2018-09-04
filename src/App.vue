6f03be2cd1b15ddab0cf09493bcc849bac197f4f
Victor Kozlov <punkrus18@gmail.com>
2018-09-04 14:32:22 +0300

    fix(socket addr)
    fix: socket addr for docker;

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import Vue from 'vue'
 export default {
   name: 'app',
   created () {
-    this.socket = socketClient('http://localhost:1734')
+    this.socket = socketClient('http://localhost:4444')
 
     this.socket.on('rooms', this.onRooms.bind(this))
     this.socket.on('new room', this.onNewRoom.bind(this))
